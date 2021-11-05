29a4f22b342a49839b37a23d122952a87d4f98fc
Ricardo Lima <ricardo.engp@gmail.com>
2021-11-05 15:59:52 -0300

    fix: fixing route documents

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import { AppProvider } from "./providers";
 
 ReactDOM.render(
   <React.StrictMode>
     {/*<ColorModeScript />*/}
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <BrowserRouter>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
