8d9007796b71e70ac7877367345577b61d5c9acb
Etnan Sousa <poketnans@gmail.com>
2021-11-09 16:15:19 -0300

    feat(router): implements the routes authentication

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,23 @@
 import * as React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 import { Home } from "../pages/Home";
+import DashboardRoute from "./dashboardRoute";
+import AuthRoute from "./routes";
 
 export const Routes = () => {
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
+      <AuthRoute exact path="/" component={Home} />
+      <AuthRoute exact isPrivate path="/dashboard" component={DashboardRoute} />
+      {/* <AuthRoute path="/login" component={Login} /> */}
+      {/* <AuthRoute path="/signup" component={RegisterUser} /> */}
+      {/* <AuthRoute path="/signup-company" component={RegisterCompany} /> */}
+      {/* <AuthRoute isPrivate path="/dashboard/user" component={UserDashboard} /> */}
+      {/* <AuthRoute
+        isPrivate
+        path="/dashboard/worker"
+        component={WorkerDashboard}
+      /> */}
     </Switch>
   );
 };
---
+++ src/routes/routes.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+interface AuthRouteProps {
+  component: React.ElementType;
+  isPrivate?: boolean;
+  path: string;
+  exact?: boolean;
+}
+
+const AuthRoute = ({
+  component: Component,
+  isPrivate,
+  ...rest
+}: AuthRouteProps) => {
+  //   const token: string | null = localStorage.getItem("@app_token");
+  const token: string | null = "token";
+
+  // a prop render trata como será a renderição do componente Route
+  // Temos 4 possibilidades de autenticação
+  // 1 - Rota privada e usuário não logado -> direcionar para a home
+  // 2 - Rota pública e usuário logado     -> direcionar para o dashboard
+  // 3 - Rota pública e usuário não logado -> Renderizar o componente
+  // 4 - Rota privada e usuário logado     -> Renderizar o componente
+
+  return (
+    <Route
+      {...rest}
+      render={() =>
+        !!isPrivate === !!token ? (
+          <Component />
+        ) : (
+          <Redirect to={isPrivate ? "/" : "/dashboard"} />
+        )
+      }
+    />
+  );
+};
+
+export default AuthRoute;
