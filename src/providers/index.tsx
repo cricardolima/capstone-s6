fc7c1ddf3c810258c3377cd71b8baadc04b4306f
Luiz Victor <luizvictornacimento00@gmail.com>
2021-11-11 11:53:18 -0300

    feature: signup provider

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,11 +1,17 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
 import { ReactNode } from "react";
 import { theme } from "../styles/theme";
+import { UserAuthProvider } from "./UserAuth";
 
 interface AppProviderProps {
   children: ReactNode;
 }
 
 export const AppProvider = ({ children }: AppProviderProps) => {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+  return (
+    <UserAuthProvider>
+      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+    </UserAuthProvider>
+  );
 };
