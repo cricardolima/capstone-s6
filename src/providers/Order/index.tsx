94adb92eccde94f2bbbef06ebf6a9b4d09479ea2
Etnan Sousa <poketnans@gmail.com>
2021-11-12 20:00:11 -0300

    feat: initializes update order implementation

--- src/providers/Order/index.tsx
+++ src/providers/Order/index.tsx
@@ -1,12 +1,14 @@
 import { useToast } from "@chakra-ui/toast";
 import { useContext, createContext, ReactNode, useState } from "react";
 import api from "../../services/api";
+import {AxiosRequestConfig} from "axios"
 
 interface IOrderProvidertData {
     userOrders: IOrderData[];
     companyOrders: IOrderData[];
     unpickedOrders: IOrderData[];
     newOrder: (body: IOrderData) => void;
+    pickupOrder: (orderId: number) => void;
 }
 
 interface IOrderProviderProps {
@@ -45,6 +47,10 @@ interface User  {
     phone?: string
 }
 
+export interface IUpdateOrderBodyData {
+    pickedUpBy?: number
+}
+
 const OrderContext = createContext<IOrderProvidertData>({} as IOrderProvidertData)
 
 export const OrderProvider = ({children}: IOrderProviderProps) => {
@@ -55,55 +61,84 @@ export const OrderProvider = ({children}: IOrderProviderProps) => {
 
     const toast = useToast()
 
+    const mainEndpoint: string = `/orders`;
+
     const getToken = () => JSON.parse(`${localStorage.getItem("@conserta:accessToken")}`) as string;
 
     const getUserInfo = () => JSON.parse(`${localStorage.getItem("@conserta:user")}`) as User;
 
     const getBearer = () => `Bearer ${getToken()}` as string;
 
+    const authorization = () => ({ headers:{ Authorization: getBearer()}}) as AxiosRequestConfig
+
     const getAllOrders = () => {
-        api.get(`/order`,{ headers:{ Authorization: getBearer()}})
+        api.get(mainEndpoint,{ headers:{ Authorization: getBearer()}})
         .then(({data}) => setAllOrders(data))
         .catch(()=> console.log("Deu Ruim"))
     }
 
-    const newOrder = (body: IOrderData) => {
-        api.post("/orders", body,{ headers:{ Authorization: getBearer()}})
-        .then(() => {
-            getAllOrders();
-            getUserOrders();
-            getUnpickedOrders();
-            toast({title:"Deu Show", status:"success", isClosable:true});
-        })
-        .catch(()=> toast({title:"Deu Ruim", status:"error", isClosable:true}))
-    }
-
     const getUserOrders = () => {
-
-        const {type} = getUserInfo();
+        const {type, id} = getUserInfo();
 
         let filteredOrders: IOrderData[];
 
         if ( type === "user" ) {
-            filteredOrders = allOrders.filter( ({userId}) => userId === getUserInfo().id );
+            filteredOrders = allOrders.filter( ({userId}) => userId === id );
 
             setUserOrders(filteredOrders);
-        } else {
-            filteredOrders = allOrders.filter( ({pickedUpBy}) => pickedUpBy === getUserInfo().id );
+        };
+        
+        if ( type === "company" ) {
+            filteredOrders = allOrders.filter( ({pickedUpBy}) => pickedUpBy === id );
 
             setCompanyOrders(filteredOrders);
         }
     }
 
     const getUnpickedOrders = () => {
-
-        const {type} = getUserInfo();
+        const {type, id} = getUserInfo();
 
         if ( type === "company" ) {
-        const filteredOrders: IOrderData[] = allOrders.filter( ({pickedUpBy}) => pickedUpBy !== getUserInfo().id );
+        const filteredOrders: IOrderData[] = allOrders.filter( ({pickedUpBy}) => pickedUpBy !== id );
 
         setUnpickedOrders(filteredOrders);
         }
+    }
+
+    const updateStates = () => {
+        getAllOrders();
+        getUserOrders();
+        getUnpickedOrders();
+    }
+
+    const newOrder = (body: IOrderData) => {
+        api.post(mainEndpoint, body,{ headers:{ Authorization: getBearer()}})
+        .then(() => {
+            updateStates();
+            toast({title:"Deu Show", status:"success", isClosable:true});
+        })
+        .catch(()=> toast({title:"Deu Ruim", status:"error", isClosable:true}))
+    }
+
+    const updateOrder = (data: IUpdateOrderBodyData) => {
+
+    }
+
+    const pickupOrder = (orderId: number) => {
+        const {id} = getUserInfo();
+
+        const endpoint: string = mainEndpoint + `/${orderId}`
+
+        const data: IUpdateOrderBodyData = {
+            pickedUpBy: id
+        }
+
+        api.patch(endpoint, data, authorization())
+        .then( () => {
+            updateStates();
+            toast({title:"Deu Show", status:"success", isClosable:true});
+        })
+        .catch(()=> toast({title:"Deu Ruim", status:"error", isClosable:true}))
     }
 
     const data = {
@@ -111,6 +146,7 @@ export const OrderProvider = ({children}: IOrderProviderProps) => {
         companyOrders,
         unpickedOrders,
         newOrder,
+        pickupOrder
     }
 
     return (
