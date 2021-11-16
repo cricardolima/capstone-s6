ca757b94067492a230d363b24b51dd75bed210e1
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2021-11-16 11:54:00 -0300

    fix: finish page register company refactor

--- src/pages/RegisterCompany/index.tsx
+++ src/pages/RegisterCompany/index.tsx
@@ -82,6 +82,28 @@ export const RegisterCompany = () => {
         padding="20px"
         boxShadow="0px 4px 8px 4px rgba(0, 0, 0, 0.25);"
       >
+              <Box width="100%" marginBottom="20px">
+                <ButtonBack />
+              </Box>
+        <Flex 
+          flexDirection={["column","row","row","row"]}
+          alignItems="center"
+          gridGap={["10px"]}
+          >
+              <Box display="flex" justifyContent="center" >
+                <Img width="80px" src={consertaLogo} alt="Conserta Logo!" />
+              </Box>
+              <Box textAlign="center" paddingBottom="20px" >
+                <Text
+                  fontSize={["lg", "2xl", "2xl"]}
+                  fontWeight="bold"
+                  color="text"
+                  textAlign={["center", "start", "start"]}
+                >
+                  Cadastro de Empresa
+                </Text>
+              </Box>
+        </Flex>
         <Flex
           width="100%"
           flexDirection={["column", "column", "row"]}
@@ -89,21 +111,8 @@ export const RegisterCompany = () => {
           gridGap="10px"
         >
           <Stack flex="1" alignItems="center">
-            <Flex
-              width="100%"
-              height="60px"
-              margin="10px 0px"
-              paddingRight="40px"
-              justifyContent="space-between"
-            >
-              <Flex flex="1" justifyContent="flex-start" alignItems="center">
-                <ButtonBack />
-              </Flex>
-              <Flex flex="1" justifyContent="flex-end" alignItems="center">
-                <Img width="80px" src={consertaLogo} alt="Conserta Logo!" />
-              </Flex>
-            </Flex>
-            <Box w="100%" maxWidth="400px" margin="0 auto">
+         
+            <Box w="100%" maxWidth="400px">
               <Input
                 placeholder="Razão Social"
                 label={"Razão Social"}
@@ -112,7 +121,7 @@ export const RegisterCompany = () => {
                 {...register("company_name")}
               />
             </Box>
-            <Box w="100%" maxWidth="400px" margin="0 auto">
+            <Box w="100%" maxWidth="400px" >
               <Input
                 placeholder="CNPJ"
                 label={"CNPJ :"}
@@ -121,7 +130,7 @@ export const RegisterCompany = () => {
                 {...register("cnpj")}
               />
             </Box>
-            <Box w="100%" maxWidth="400px" margin="0 auto">
+            <Box w="100%" maxWidth="400px" >
               <Input
                 placeholder="Nome Fantasia"
                 label={"Nome Fantasia :"}
@@ -129,7 +138,7 @@ export const RegisterCompany = () => {
                 {...register("name")}
               />
             </Box>
-            <Box w="100%" maxWidth="400px" margin="0 auto">
+            <Box w="100%" maxWidth="400px" >
               <Input
                 placeholder="(85) 9 1234-5678 "
                 label={"Telefone:"}
@@ -142,24 +151,7 @@ export const RegisterCompany = () => {
           </Stack>
 
           <Stack flex="1">
-            <Flex
-              width="100%"
-              height="60px"
-              margin="10px 0px"
-              justifyContent="space-between"
-            >
-              <Flex flex="1" justifyContent="center" alignItems="center">
-                <Text
-                  fontSize={["lg", "2xl", "2xl"]}
-                  fontWeight="bold"
-                  color="text"
-                  textAlign={["center", "start", "start"]}
-                >
-                  Cadastro de Empresa ou Empreendedor Individual
-                </Text>
-              </Flex>
-            </Flex>
-            <Box w="100%" maxWidth="400px" margin="0 auto">
+            <Box w="100%" maxWidth="400px">
               <Input
                 placeholder="Email"
                 label={"Email :"}
@@ -174,7 +166,7 @@ export const RegisterCompany = () => {
                 </Text>
               )}
             </Box>
-            <Box w="100%" maxWidth="400px" margin="0 auto">
+            <Box w="100%" maxWidth="400px" >
               <Input
                 placeholder="Senha"
                 label={"Senha :"}
@@ -184,7 +176,7 @@ export const RegisterCompany = () => {
                 icon={FaLock}
               />
             </Box>
-            <Box w="100%" maxWidth="400px" margin="0 auto">
+            <Box w="100%" maxWidth="400px" >
               <Input
                 placeholder="Confirmação de Senha"
                 label={"Confirmação de Senha :"}
