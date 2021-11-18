8b58e0c9b7b73672dfe1017655b599a236b23d6c
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2021-11-18 18:01:24 -0300

    feat: test input

--- src/__tests__/components/Input/Input.spec.js
+++ src/__tests__/components/Input/Input.spec.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen,fireEvent } from "@testing-library/react";
 import { Input } from "../../../components/Input";
 
 describe("Input component", () => {
@@ -7,4 +7,10 @@ describe("Input component", () => {
     render(<Input name={"Telefone"} label={"Phone"} />);
     expect(screen.getByLabelText("Phone")).toBeTruthy();
   });
+  test("should be able insert values on input",async()=>{
+    render(<Input name={"Telefone"} label={"Phone"} />);
+      const input = screen.getAllByPlaceholderText("Phone");
+      await fireEvent.change(input,{target:{value:"85999987538"}})
+        expect(input).toHaveTextContent("85999987538")
+})
 });
