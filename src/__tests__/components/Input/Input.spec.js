888fc67b68a106dd2b312baf05465b8be6c3daaa
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2021-11-18 18:23:34 -0300

    feat: more test input

--- src/__tests__/components/Input/Input.spec.js
+++ src/__tests__/components/Input/Input.spec.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen,fireEvent } from "@testing-library/react";
+import { render, screen,fireEvent,waitFor} from "@testing-library/react";
 import { Input } from "../../../components/Input";
 
 describe("Input component", () => {
@@ -7,10 +7,10 @@ describe("Input component", () => {
     render(<Input name={"Telefone"} label={"Phone"} />);
     expect(screen.getByLabelText("Phone")).toBeTruthy();
   });
-  test("should be able insert values on input",async()=>{
-    render(<Input name={"Telefone"} label={"Phone"} />);
-      const input = screen.getAllByPlaceholderText("Phone");
-      await fireEvent.change(input,{target:{value:"85999987538"}})
-        expect(input).toHaveTextContent("85999987538")
+  test("should be able insert values on input",  ()=>{
+    render(<Input name={"Telefone"} placeholder="Phone" label={"Phone"} />);
+      const input = screen.getByPlaceholderText("Phone");
+      fireEvent.change(input,{target:{value:"85999987538"}})
+      expect(input).toHaveValue("85999987538")  
 })
 });
